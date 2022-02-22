6884bdb61dad111d4a0c78b4cb2683dd1ec19132
fcoverdugo <fcoverdugoa@underpost.net>
2022-02-22 11:07:54 -0300

    update

---
+++ src/client/views/editor.js
@@ -0,0 +1,225 @@
+import { Rest } from '../mods/rest.js';
+import { UnderpostQuillEditor } from '../mods/underpost-quill-editor.js';
+import { UnderpostInteract } from '../mods/underpost-interact.js';
+import { Base } from '../mods/base.js';
+
+
+class Editor {
+  constructor(){
+
+    new Base();
+
+    const sizeTitle = 15;
+    const sizeContent = 15;
+
+    append('body', renderInput({
+      underpostClass: 'in',
+      id_content_input: 'a1',
+      id_input: 'underpost-ql-title',
+      type: 'text',
+      required: true,
+      style_content_input: '',
+      style_input: `
+
+          padding: 12px 15px;
+          font-family: retro-font;
+          font-size: `+sizeTitle+`px;
+
+      `,
+      style_label: 'color: red; font-size: 12px;',
+      style_outline: true,
+      style_placeholder: 'font-style: italic;',
+      textarea: false,
+      active_label: false,
+      initLabelPos: 3,
+      endLabelPos: -25,
+      text_label: 'Title',
+      tag_label: 'a3',
+      fnOnClick: async () => {
+        console.log('click input');
+      },
+      value: ``,
+      topContent: '',
+      botContent: '',
+      placeholder: 'Title'
+    }));
+
+
+
+
+      const renderTitle = obj_ => `
+
+           <div class='in' style='color: white; background: red; font-size: 20px; padding: 5px;'>
+               `+obj_.title+`
+           </div>
+           <div class='in' style='color: white; background: red; font-size: 18px; padding: 5px;'>
+               `+obj_.date.split('.')[0].replace('T', ' ')+`
+           </div>
+
+      `;
+      this.editor = new UnderpostQuillEditor({
+          divContent: 'body',
+          style:  {
+              tr: `
+                background: none;
+              `,
+              td: `
+                min-width: 100px;
+                border: 1px solid gray;
+              `,
+              ql_editor: `
+                min-height: 300px;
+                background: #ebeceb;
+                overflow: hidden !important;
+              `,
+              standalone_container: `
+                background: #c7c9c7;
+                color: black;
+              `,
+              placeholder: `
+                color: red;
+                content: attr(data-placeholder);
+                font-style: normal;
+                left: 15px;
+                pointer-events: none;
+                position: absolute;
+                right: 15px;
+                font-style: italic;
+              `
+          },
+          fontDefault: 'retro-font',
+          fonts: ['gothic', 'retro-font', 'arial', 'Verdana', 'Times'],
+          placeholder: 'Compose an epic...',
+          initContent: '',
+          sizeDefault: sizeContent,
+          text_sizes: range(1, 80).filter(size_=>size_%5==0).map(size_=>size_+'px'),
+          scientific_tools: true,
+          image: true,
+          video: true,
+          table: true,
+          idBtnSend: '.btn-send-underpost',
+          interactQuill: new UnderpostInteract( {
+            type: 'quill'
+          }),
+          onSubmit: async (value, quillLength) => {
+
+                        const title = s('.underpost-ql-title').value;
+
+                           if(quillLength<=1){
+                             return   notifi.display(
+                                'rgb(22, 22, 22)',
+                                'Empty content',
+                                2000,
+                                'error'
+                              );
+                           }
+
+                           if(title==''){
+                             return   notifi.display(
+                                'rgb(22, 22, 22)',
+                                'Empty title',
+                                2000,
+                                'error'
+                              );
+                           }
+
+                           value = value.replace('contenteditable="true"', 'style="background: none"');
+                           // value = value.replaceAll('transform: translate', 'none: ');
+                           value = value.replace('ql-editor-main', '');
+
+                           const date = new Date().toISOString();
+
+                           prepend('.'+idContentDashBoard, renderTitle({title, date}) + value);
+
+                           const dataPost  = {
+                             title,
+                             date,
+                             base64Html: enBase64(value),
+                             id: getHash().replaceAll('-', '')
+                           };
+
+                           const response = await new Rest().FETCH('/posts', 'post', dataPost);
+
+                           console.log('POST /posts ->');
+                           console.log(response);
+
+                           if(response.success===true){
+                             this.editor.reset();
+                             return notifi.display(
+                                'rgb(22, 22, 22)',
+                                'Success',
+                                2000,
+                                'success'
+                              );
+                           }
+
+                           return notifi.display(
+                              'rgb(22, 22, 22)',
+                              'Error service',
+                              2000,
+                              'error'
+                            );
+          }
+      });
+
+      s('.ql-editor').classList.add('ql-editor-main');
+
+
+      const idContentDashBoard = 'dashboard-post';
+
+
+      append('body', `
+
+
+
+          <style>
+
+                .btn-send-underpost {
+
+                  transition: .3s;
+                  padding: 15px;
+                  margin: 5px;
+                  background: rgba(212, 0, 0, 0.8);
+                  cursor: pointer;
+                  font-size: 13px;
+                  font-family: retro-font;
+
+                }
+                .btn-send-underpost:hover {
+                  background: rgba(212, 0, 0, 1);
+                }
+          </style>
+
+          <div class='inl btn-send-underpost'>
+
+                SEND
+
+          </div>
+
+          <div class='in `+idContentDashBoard+`'>
+
+          </div>
+
+
+
+      `);
+
+
+      (async ()=>{
+
+          const currentsPost = await new Rest().FETCH('/posts', 'get');
+          console.log('currentsPost ->');
+          console.log(currentsPost);
+          console.log('currentsPost Size ->');
+          console.log(getSizeJSON(currentsPost));
+
+          currentsPost.data.reverse().map(dataPost =>
+            append('.'+idContentDashBoard, renderTitle(dataPost)+deBase64(dataPost.base64Html))
+          );
+
+      })();
+
+  }
+}
+
+new Editor();
